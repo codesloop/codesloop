dbf49c5394cf03c5206b8dd9ec4e2d4b922ac13a
David Beck <dbeck@beckground.hu>
2009-08-09 15:42:00 +0200

    fix more licenses

--- slt3/src/var.hh
+++ slt3/src/var.hh
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2008,2009, David Beck, Tamas Foldi
+Copyright (c) 2008,2009, CodeSLoop Team
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
