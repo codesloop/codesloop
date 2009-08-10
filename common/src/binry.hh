4387b785ce4d335914c7cb02117dc0a632ba480f
David Beck <dbeck@beckground.hu>
2009-08-10 18:44:28 +0200

    fix more licenses

--- common/src/binry.hh
+++ common/src/binry.hh
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2008,2009, Tamas Foldi, David Beck
+Copyright (c) 2008,2009, CodeSLoop Team
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
